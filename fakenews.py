165ad99dc9f2ed4b55b63792b1f21832a184490e
Billy <billym@gec.inatel.br>
2020-08-22 15:02:30 -0300

    Adicionando os filtros : lematização/stemização e stopword com NLTK

--- fakenews.py
+++ fakenews.py
@@ -1,10 +1,16 @@
 import pandas as pd
+import nltk
+import csv
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.ensemble import IsolationForest
 from sklearn.covariance import EllipticEnvelope
 from sklearn.neighbors import LocalOutlierFactor
 from sklearn.svm import OneClassSVM
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer
+from nltk.stem import WordNetLemmatizer
 
 df = pd.read_csv("https://s3.amazonaws.com/assets.datacamp.com/blog_assets/fake_or_real_news.csv")
 
@@ -18,28 +24,53 @@ result = df.label
 newDf = df.drop(columns="label")
 #newDf = df.drop("label", axis=1)
 
-print('Retirando caracteres estranhos')
-# Retirando caracteres estranhos e espaços
+# Filtro 1
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('wordnet')
+
 for k, t in enumerate(df['text']):
-    df['text'][k] = ''.join((c for c in t if c.isalnum())).lower()
+    if k == 4:
+        lemmatizer = WordNetLemmatizer()
+        ps = PorterStemmer()
+        stop_words = set(stopwords.words('english'))
+        dataset_sent = df['text'][k] = ''.join((c for c in t))
+        print(dataset_sent, '\n')
+
+        print('* Fazendo a lematização da', k ,'ª noticia \n')
+        dataset_sent = lemmatizer.lemmatize(dataset_sent, pos='a')
+
+        print('* Removendo Stop Words e fazendo a stemização da', k ,'ª noticia \n')
+        word_tokens = word_tokenize(dataset_sent)
+        filtered_sentence = [w for w in word_tokens if not w in stop_words]
+        filtered_sentence = []
+
+        for w in word_tokens:
+            if w not in stop_words:
+                filtered_sentence.append(ps.stem(w))
+
+        print('* Retirando caracteres estranhos e espaços \n')
+        df['text'][k] = ''.join(c for c in filtered_sentence if c.isalnum()).lower()
+        print(df['text'][k])
 
 print(df['text'])
 
 # X -> notícias y-> resultados(fake or real)
 X_train, X_test, y_train, y_test = train_test_split(df['text'], result, test_size=0.30, random_state=53)
 
-# Filtro 1
+# Filtro 2 (Feito acima se quiser pode remover)
 # Remove os english stop words
-count_vectorizer = CountVectorizer(stop_words='english')
-new_train = count_vectorizer.fit_transform(X_train)
-new_test = count_vectorizer.transform(X_test)
+# count_vectorizer = CountVectorizer(stop_words='english')
+# new_train = count_vectorizer.fit_transform(X_train)
+# new_test = count_vectorizer.transform(X_test)
 
-# Filtro 2
+# Filtro 3
 # Usando max_df
 # Remove palavras que aparecem em mais de 40% das notícias
-tfidf_vectorizer = TfidfVectorizer(stop_words='english', max_df=0.4)
-tfidf_train = tfidf_vectorizer.fit_transform(X_train)
-tfidf_test = tfidf_vectorizer.transform(X_test)
+# tfidf_vectorizer = TfidfVectorizer(stop_words='english', max_df=0.4)
+# tfidf_train = tfidf_vectorizer.fit_transform(X_train)
+# tfidf_test = tfidf_vectorizer.transform(X_test)
+
 
 # train_matrix = new_train.toarray()
 # test_matrix = new_test.toarray()
