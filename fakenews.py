48aa8c7c052b0640624d805ac9883f553cee63ab
brenerovl <brener.oliveira@gec.inatel.br>
2020-08-31 19:59:24 -0300

    Ajuste na lemmatização

--- fakenews.py
+++ fakenews.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import nltk
 import csv
-import wandb
+# import wandb
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
@@ -14,12 +14,14 @@ from nltk.tokenize import word_tokenize
 from nltk.stem import PorterStemmer
 from nltk.stem import WordNetLemmatizer
 
-wandb.init(project="test-project")
+# wandb.init(project="test-project")
 
-wandb.config.test_size = 0.3
-wandb.config.seed = 0
+# wandb.config.test_size = 0.3
+# wandb.config.seed = 0
+test_size = 1
+seed = 53
 
-df = pd.read_csv("https://s3.amazonaws.com/assets.datacamp.com/blog_assets/fake_or_real_news.csv")
+df = pd.read_csv("./assets/fake_or_real_news.csv")
 news_content_test = []
 result_content_test = []
 
@@ -31,28 +33,26 @@ nltk.download('stopwords')
 nltk.download('punkt')
 nltk.download('wordnet')
 
+lemmatizer = WordNetLemmatizer()
+stop_words = set(stopwords.words('english'))
+
 for k, t in enumerate(df['text']):
-    if k <= 4:
-        lemmatizer = WordNetLemmatizer()
-        ps = PorterStemmer()
-        stop_words = set(stopwords.words('english'))
-
-        dataset_sent = str(t)
-        #print(dataset_sent, '\n')
-
-        print('* Fazendo a lematização da', k ,'ª noticia')
-        dataset_sent = lemmatizer.lemmatize(dataset_sent, pos='a')
-
-        print('* Removendo Stop Words e fazendo a stemização da', k ,'ª noticia')
-        word_tokens = word_tokenize(dataset_sent)
-
-        filtered_sentence = []
-
+    if k < 4:
+        print('* Fazendo a lematização e retirando caracteres da', k ,'ª noticia')
+        word_tokens = word_tokenize(t)
+        word_filtered = []
         for w in word_tokens:
-            if w not in stop_words:
-                filtered_sentence.append(ps.stem(w))
+            w = ''.join(c for c in w if c.isalnum()).lower()
+            w = lemmatizer.lemmatize(w, pos='v')
+            word_filtered.append(w)
+
+        filtered_sentence = ' '.join(w for w in word_filtered)
+        print('* Removendo Stop Words da', k ,'ª noticia')
+        word_tokens = word_tokenize(filtered_sentence)
+        sentence_not_stop_word = ' '.join(w for w in word_tokens if not w in stop_words)
+
         print('* Retirando caracteres estranhos e espaços \n')
-        news_content_test.append(' '.join(c for c in filtered_sentence if c.isalnum()))
+        news_content_test.append(sentence_not_stop_word)
         result_content_test.append(df['label'][k])
 
 # print(df['text'])
@@ -79,28 +79,27 @@ for k, t in enumerate(df['text']):
 #newDf = df.drop("label", axis=1)
 
 # X -> notícias y-> resultados(fake or real)
-X_train, X_test, y_train, y_test = train_test_split(news_content_test, result_content_test, test_size=wandb.config.test_size, random_state=wandb.config.seed)
+# X_train, X_test, y_train, y_test = train_test_split(news_content_test, result_content_test, test_size=test_size, random_state=seed)
 
-new_test = X_test
+# new_test = X_test
 
-train_matrix = np.array(X_train).reshape(-1, 1)
-test_matrix = new_test
-
+# train_matrix = np.array(X_train).reshape(-1, 1).astype(np.float64)
+# test_matrix = new_test
 
 # IsolationForest
 # Treino
-print('* Treinando modelo com IsolationForest')
-isolationModel = IsolationForest(random_state=0).fit(train_matrix) # Verificar random state
+# print('* Treinando modelo com IsolationForest')
+# isolationModel = IsolationForest(random_state=0).fit(train_matrix) # Verificar random state
 # Teste
 # Deve ser comparado com o y_test
 
-print('* Testando modelo')
-isolationTestResult = isolationModel.predict(test_matrix)
+# print('* Testando modelo')
+# isolationTestResult = isolationModel.predict(test_matrix)
 
-wandb.log({'test': isolationTestResult})
-print(isolationTestResult)
+# wandb.log({'test': isolationTestResult})
+# print(isolationTestResult)
 
-wandb.sklearn.plot_class_proportions(y_train, y_test, ['TRUE', 'FAKE'])
+# wandb.sklearn.plot_class_proportions(y_train, y_test, ['TRUE', 'FAKE'])
 
 # EllipticEnvelope
 
