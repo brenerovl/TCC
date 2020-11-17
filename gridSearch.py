f9a6ec6d70bdf40b6e7f83ba46fc02ca18455a67
Billy <billym@gec.inatel.br>
2020-11-17 20:45:25 -0300

    Adicionando o gridSearch para o Elliptic Envelope

--- gridSearch.py
+++ gridSearch.py
@@ -2,6 +2,7 @@ from enum import auto
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import GridSearchCV
+from sklearn.covariance import EllipticEnvelope
 from sklearn.dummy import DummyRegressor
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import f1_score, make_scorer
@@ -12,10 +13,7 @@ from processamentoDataset import pre_processamento
 if __name__ == "__main__":
     # https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.GridSearchCV.html
     
-    housing = datasets.load_boston()
     iris = datasets.load_iris()
-    X = housing.data
-    y = housing.target
     # train, test = pre_processamento()
 
     #   https://scikit-learn.org/stable/modules/generated/sklearn.metrics.explained_variance_score.html#sklearn.metrics.explained_variance_score
@@ -25,26 +23,37 @@ if __name__ == "__main__":
     #   https://scikit-learn.org/stable/modules/generated/sklearn.metrics.r2_score.html#sklearn.metrics.r2_score
     #   https://scikit-learn.org/stable/modules/generated/sklearn.metrics.median_absolute_error.html#sklearn.metrics.median_absolute_error
     #   Métricas que serão medidas
-    f1 = make_scorer(f1_score , average='weighted')
-    metricas = ['explained_variance', 'neg_mean_absolute_error', 'neg_mean_squared_error', 'neg_mean_squared_log_error', 'neg_median_absolute_error', 'r2' , 'f1']
+    macroF1 = make_scorer(f1_score , average='macro')
+    microF1 = make_scorer(f1_score , average='micro')
+    weightedF1 = make_scorer(f1_score , average='weighted')
+    EEgridSearchParameters = {"contamination": np.linspace(0.0, 1, 200)} 
+    OSVMgridSearchParameters = {'C': [1, 10, 100, 1000], 'gamma': [0.1, 0.01, 0.001, 0.0001, 0.00001,  'auto', 'scale'], 'kernel': ['linear', 'poly', 'rbf', 'sigmoid'], 'degree':[2, 3, 4]}
 
-    # Utilizando um classificador (SVC) (sklearn indica esse)
-    svc = svm.SVC()
+    # metricas = ['explained_variance', 'neg_mean_absolute_error', 'neg_mean_squared_error', 'neg_mean_squared_log_error', 'neg_median_absolute_error', 'r2' , 'f1']
     # print(svc.get_params().keys())
-    # Parametros que podem ser usados :
-    # dict_keys(['C', 'break_ties', 'cache_size', 'class_weight', 'coef0', 'decision_function_shape', 
-    # 'degree', 'gamma', 'kernel', 'max_iter', 'probability', 'random_state', 'shrinking', 'tol', 'verbose'])
-    parameters = {'C': [1, 10, 100, 1000], 'gamma': [0.1, 0.01, 0.001, 0.0001, 0.00001,  'auto', 'scale'], 'kernel': ['linear', 'poly', 'rbf', 'sigmoid'], 'degree':[2, 3, 4]}
+
+    #Rodando o GridSearchCV para o One Class SVM
     # clf = GridSearchCV(svc, parameters , scoring=metricas, verbose=100, refit='f1')
-    clf = GridSearchCV(svc, parameters , scoring= f1, verbose=100)
-    clf.fit(iris.data, iris.target)
-    clf.best_estimator_
-    print(clf.best_estimator_)
-    clf.best_score_
-    print(clf.best_score_)
+    svc = svm.SVC()
+    OSVM = GridSearchCV(svc, OSVMgridSearchParameters , scoring= macroF1 , verbose=100)
+    OSVM.fit(iris.data, iris.target)
+    OSVM.best_estimator_
+    print(OSVM.best_estimator_)
+    OSVM.best_score_
+    print(OSVM.best_score_)
+
+    #Rodando o GridSearchCV para o Elliptic Envelope
+    ell = EllipticEnvelope()
+    EE = GridSearchCV(ell, EEgridSearchParameters , scoring= macroF1 , verbose=100)
+    EE.fit(iris.data, iris.target)
+    EE.best_estimator_
+    print(EE.best_estimator_)
+    EE.best_score_
+    print(EE.best_score_)
     # A melhor estratégia foi a , a previsão do modelo foi a mediana dos valores de treinamento pra toda casa nova. A gente pega a diferença do prebisto pelo verdadeiro, eleva ao quadrado (normal em estatística pra evitar que erros negativos e positivos se anulem, e é mais fácil de derivar do que usar módeulo).
     # Então pra ter uma ideia se isso ta bom ou ruim basta tirar a raiz disso:
     # np.sqrt(clf.best_score_*-1)
     # print(np.sqrt(clf.best_score_*-1))
-    clf.cv_results_
+    OSVM.cv_results_
+    EE.cv_results_
     # print(clf.cv_results_)
